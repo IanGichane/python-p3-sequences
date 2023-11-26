e7a0c1a0f114181b2957d03709aa814afa982efd
IanGichane <gichaneian@gmail.com>
2023-11-26 22:17:00 +0300

    fib sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib=[0,1]
+    for i in range(2,length):
+        fib.append(fib[i-1]+fib[i-2])
+    print(fib)
+       
+
+print_fibonacci(9)
