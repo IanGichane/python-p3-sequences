475c5006fac504000f69bc92531f80972ef5b09b
IanGichane <gichaneian@gmail.com>
2023-11-26 22:25:57 +0300

    fibanocci

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,9 +2,11 @@
 
 def print_fibonacci(length):
     fib=[0,1]
-    for i in range(2,length):
-        fib.append(fib[i-1]+fib[i-2])
-    print(fib)
+    while length !=0 and length>0:
+        for i in range(2,length):
+            fib.append(fib[i-1]+fib[i-2])
+        print(fib)
+    print('Empty list')
        
 
-print_fibonacci(9)
+print_fibonacci(0)
